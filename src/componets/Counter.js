b175de13d44bfe0231fdfbd8557321f4faf57e42
Jorge Angel Novoa Hernandez <84944519+Angel25Novoa@users.noreply.github.com>
2022-05-31 19:19:04 -0500

    Update Counter.js

--- src/componets/Counter.js
+++ src/componets/Counter.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Counter = ({value = 10}) => {
   const [count, setCount] = useState(0);
 
   const suma = () => {setCount((numero) => numero +1)}
+  
+  useEffect()=>{console.log(count)}, [count])
 
   return(
     <>
